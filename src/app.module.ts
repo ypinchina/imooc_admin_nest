4db71640d521bfc636b432ad42d53c8ef5273be4
Yip <batmanyp@foxmail.com>
2026-08-22 13:31:38 +0800

    feat: 连接本地mysql数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,26 @@ import { AuthModule } from './modules/auth/auth.module';
 import { BookModule } from './modules/book/book.module';
 import { AuthController } from './modules/auth/auth.controller';
 import { BookController } from './modules/book/book.controller';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as path from 'path';
+import * as os from 'os';
+import * as fs from 'fs';
+const homedir = os.homedir();
+const usernamePath = path.resolve(homedir, 'vben', 'username');
+const username = fs.readFileSync(usernamePath).toString();
+const passwordPath = path.resolve(homedir, 'vben', 'password');
+const password = fs.readFileSync(passwordPath).toString();
 @Module({
-  imports: [UserModule, AuthModule, BookModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username,
+      password,
+      host: 'localhost',
+      port: 3306
+    }),
+    UserModule, AuthModule, BookModule,],
   controllers: [AppController, AuthController, BookController],
   providers: [AppService, TestService],
 })
-export class AppModule {}
+export class AppModule { }
